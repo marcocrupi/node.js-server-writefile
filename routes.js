ddceff6eb836e22b35c3014949a08dbf813080d0
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-10-18 09:15:41 +0200

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</head>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+      const message = parseBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</head>");
+  res.end();
+};
+
+// EXPORT - FIRST METHOD
+// module.exports = requestHandler;
+
+// EXPORT FOR MULTIPLE ELEMENT - SECOND METHOD
+module.exports = {
+  handler: requestHandler,
+  someText: "Some hard coded text",
+};
+
+// EXPORT FOR MULTIPLE ELEMENT - THIRD METHOD
+// module.exports.handler = requestHandler;
+// module.exports.someText = "Some hard coded text";
+
+// EXPORT FOR MULTIPLE ELEMENT - FOURTH METHOD
+// exports.handler = requestHandler;
+// exports.someText = "Some hard coded text";
